fa775e39403ca8d448dc04bdef9e6b19000570bf
Neeraj Kumar <neerajpkumar@gmail.com>
2015-05-08 16:42:31 -0700

    Using only English set

--- CharacterSets.py
+++ CharacterSets.py
@@ -1,6 +1,15 @@
 class CharacterSetMapper():
 	def __init__(self):
-		self.mappings = {}
+		self.mappings = {
+			"a": 40, "b": 60, "c": 44, "d": 46,
+			"e": 42, "f": 64, "g": 66, "h": 62,
+			"i": 24, "j": 26, "k": 50, "l": 70,
+			"m": 54, "n": 56, "o": 52, "p": 74,
+			"q": 76, "r": 72, "s": 34, "t": 36,
+			"u": 51, "v": 71, "w": 27, "x": 55,
+			"y": 57, "z": 53,
+		}
+
 
 	def getConfig(self, side, char):
 		if side == 0:
