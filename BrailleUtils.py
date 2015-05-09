cf1604f8c8311f60de98d52c96d3d1805a599490
Neeraj Kumar <neerajpkumar@gmail.com>
2015-05-09 05:46:26 -0700

    Adding BrailleEmbosser class which controls solenoid on a specific pin. 2. Adding thread syncrhoization in the actuator while using the new braille embosser to enable embossing once the motors get to the correct state

---
+++ BrailleUtils.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+class BrailleEmbosser():
+	def __init__(self, pin=21):
+		self.pin = 21
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.pin, GPIO.OUT)
+
+	def emboss(self):
+		GPIO.output(self.pin, GPIO.HIGH)
+		time.sleep(0.5)
+		GPIO.output(self.pin, GPIO.LOW)
+
+		
--- actuator_test.py
+++ actuator_test.py
@@ -1,5 +1,6 @@
 from BrailleStepper import BrailleStepper, BrailleWheel
 from CharacterSets import EnglishCharacterSet
+from BrailleUtils import BrailleEmbosser
 
 import sys
 import time
@@ -7,7 +8,8 @@ import threading
 import traceback
 
 
-def changeStepperState(stepperMotor, toChar, charSet, side):
+
+def changeStepperState(stepperMotor, toChar, charSet, side, readyEvent):
 	tb=""
 	try:
 		print("Changing stepper motor state to " + toChar)
@@ -18,11 +20,19 @@ def changeStepperState(stepperMotor, toChar, charSet, side):
 		print("Oops something weird happened. %s" % e)
 	finally:
 		print tb
+		readyEvent.set()
+		print("Ready event triggered for " + "left" if side == 0 else "right")
+
+def punchBraille(leftStepperReady, rightStepperReady, brailleEmbosser):
+	leftStepperReady.wait()
+	rightStepperReady.wait()
+	brailleEmbosser.emboss()
 
 
 wheel = BrailleWheel()
 leftStepper = BrailleStepper(brailleWheel=wheel, onPort=[18,23,24,25])
 rightStepper = BrailleStepper(brailleWheel=wheel, onPort=[4,17,27,22])
+brailleEmbosser = BrailleEmbosser(pin=21)
 
 left_motor_thread = threading.Thread()
 right_motor_thread = threading.Thread()
@@ -34,10 +44,13 @@ while(True):
 	if user_command=="exit":
 		break
 	else:
+		leftStepperReady = threading.Event()
+		rightStepperReady = threading.Event()
 		if not left_motor_thread.isAlive():
-			left_motor_thread = threading.Thread(target=changeStepperState, args=(leftStepper, str(user_command), EN_charSet, 0,))
+			left_motor_thread = threading.Thread(target=changeStepperState, args=(leftStepper, str(user_command), EN_charSet, 0,leftStepperReady,))
 			left_motor_thread.start()
 		if not right_motor_thread.isAlive():
-			right_motor_thread = threading.Thread(target=changeStepperState, args=(rightStepper, str(user_command), EN_charSet, 1))
+			right_motor_thread = threading.Thread(target=changeStepperState, args=(rightStepper, str(user_command), EN_charSet, 1, rightStepperReady,))
 			right_motor_thread.start()
+		punchBraille(leftStepperReady, rightStepperReady, brailleEmbosser)
 	time.sleep(1)
