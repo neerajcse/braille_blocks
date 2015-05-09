373a4d185f37bc9bafe741e14d2a7eeeabc25e00
Neeraj Kumar <neerajpkumar@gmail.com>
2015-05-08 23:19:26 -0700

    New version for stepper

---
+++ stepper.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+#--------------------------------------
+#    ___  ___  _ ____          
+#   / _ \/ _ \(_) __/__  __ __ 
+#  / , _/ ___/ /\ \/ _ \/ // / 
+# /_/|_/_/  /_/___/ .__/\_, /  
+#                /_/   /___/   
+#
+#    Stepper Motor Test
+#
+# A simple script to control
+# a stepper motor.
+#
+# Author : Matt Hawkins
+# Date   : 20/03/2015
+#
+# http://www.raspberrypi-spy.co.uk/
+#
+#--------------------------------------
+#!/usr/bin/env python
+
+# Import required libraries
+import sys
+import time
+import RPi.GPIO as GPIO
+import atexit
+
+
+def cleanup():
+  GPIO.cleanup()
+  print("Cleanup complete***********")
+
+atexit.register(cleanup)
+
+
+
+# Use BCM GPIO references
+# instead of physical pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# Define GPIO signals to use
+# Physical pins 11,15,16,18
+# GPIO17,GPIO22,GPIO23,GPIO24
+StepPins = [18,27,22,23]
+
+# Set all pins as output
+for pin in StepPins:
+  print "Setup pins"
+  GPIO.setup(pin,GPIO.OUT)
+  GPIO.output(pin, False)
+
+# Define advanced sequence
+# as shown in manufacturers datasheet
+Seq = [[1,0,0,0],
+       [1,1,0,0],
+       [0,1,0,0],
+       [0,1,1,0],
+       [0,0,1,0],
+       [0,0,1,1],
+       [0,0,0,1],
+       [1,0,0,1]]
+       
+StepCount = len(Seq)-1
+StepDir = 2 # Set to 1 or 2 for clockwise
+            # Set to -1 or -2 for anti-clockwise
+
+
+# Initialise variables
+StepCounter = 0
+MaxSteps = int(sys.argv[1]) if len(sys.argv) >= 2 else 4
+print "Max steps: %i" % MaxSteps
+TotalSteps = 0
+
+# Start main loop
+while TotalSteps < MaxSteps:
+
+  for pin in range(0, 4):
+    xpin = StepPins[pin]
+    if Seq[StepCounter][pin]!=0:
+      GPIO.output(xpin, True)
+    else:
+      GPIO.output(xpin, False)
+
+  StepCounter += StepDir
+  TotalSteps += StepDir
+
+  # If we reach the end of the sequence
+  # start again
+  if (StepCounter>=StepCount):
+    StepCounter = 0
+  if (StepCounter<0):
+    StepCounter = StepCount
+
+  # Wait before moving on
+  time.sleep(0.0025)
