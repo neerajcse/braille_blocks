5def2dbeff3e50fe95c2a64d55018b3fe746a0c9
Neeraj Kumar <neerajpkumar@gmail.com>
2015-12-14 19:01:53 -0800

    Adding code to work with the square wheel configuration created in last commit

---
+++ SquareWheelBrailleStepper.py
@@ -0,0 +1,72 @@
+from ULNStepper import ULNStepper
+import atexit
+
+class MotorStates():
+	NO_PINS_00 = 0
+	ONE_PIN_01 = 1
+	ONE_PIN_10 = 2
+	TWO_PIN_11 = 3
+
+class BrailleWheel():
+	CONFIGURATION = [
+                        MotorStates.TWO_PIN_11,
+                        MotorStates.ONE_PIN_01,
+						MotorStates.NO_PINS_00,
+						MotorStates.ONE_PIN_10,
+					]
+
+
+class BrailleStepper():
+	def __init__(self,
+		brailleWheel, 
+		stepsPerRev=4076,
+		onPort=[18,27,22,23],
+		debug=False):
+
+		# register destruction steps
+		atexit.register(self.turnOffMotors)
+
+		self.debug = debug
+
+		# initalize hardward only if its in production
+		if not debug:
+			self.stepper = ULNStepper(in1=onPort[0], in2=onPort[1], in3=onPort[2], in4=onPort[3])
+			#self.stepper.setSpeed(15)
+			
+		# motor related config
+		self.STEPS_FOR_90_DEGREES = 511
+		
+		# state related config (the state of where the motor is currently)
+		self.state = MotorStates.ONE_PIN_11
+
+		# the configuration of the braille wheel that is connected to the motor.
+		self.config = brailleWheel.CONFIGURATION
+		
+
+	def goToState(self, toState):
+		currentIndex = self.config.index(self.state)
+		goToIndex = self.config.index(toState)
+		indexDiff = goToIndex - currentIndex
+		
+		print("Current:{}, To: {}, Diff: {}".format(currentIndex, goToIndex, indexDiff))
+		
+		
+		stepsToTake = (indexDiff * self.STEPS_FOR_90_DEGREES)
+		
+		# update internal state
+		self.state = toState
+
+		# recalculate stepsToTake to maintain accuracy.
+		print("BrailleStepper: Taking {0} 90 degree state changes equalling {1} steps taken".format(indexDiff, stepsToTake))
+		if not self.debug:
+			self.stepper.takeSteps(int(stepsToTake))
+		
+
+	def turnOffMotors(self):
+		if not self.debug:
+			#for i in range(1,5):
+			#	self.motorHAT.getMotor(i).run(Adafruit_MotorHAT.RELEASE)
+			del self.stepper
+			print("Turning off")
+		else:
+			print("Turning off motors")
---
+++ square_wheel_actuator_test.py
@@ -0,0 +1,56 @@
+from SquareWheelBrailleStepper import BrailleStepper, BrailleWheel
+from BinaryCharacterSet import BinaryCharacterSetMapper
+from BrailleUtils import BrailleEmbosser
+
+import sys
+import time
+import threading
+import traceback
+
+
+
+def changeStepperState(stepperMotor, toChar, charSet, side, readyEvent):
+	tb=""
+	try:
+		print("Changing stepper motor state to " + toChar)
+		toState = charSet.getConfig(side, toChar)
+		stepperMotor.goToState(toState)
+	except Exception, e:
+		tb=traceback.format_exc()
+		print("Oops something weird happened. %s" % e)
+	finally:
+		print tb
+		readyEvent.set()
+		print("Ready event triggered for " + "left" if side == 0 else "right")
+
+def punchBraille(leftStepperReady, rightStepperReady, brailleEmbosser):
+	leftStepperReady.wait()
+	rightStepperReady.wait()
+	brailleEmbosser.emboss()
+
+
+wheel = BrailleWheel()
+leftStepper = BrailleStepper(brailleWheel=wheel, onPort=[18,23,24,25])
+rightStepper = BrailleStepper(brailleWheel=wheel, onPort=[4,17,27,22])
+brailleEmbosser = BrailleEmbosser(pin=21)
+
+left_motor_thread = threading.Thread()
+right_motor_thread = threading.Thread()
+BINARY_charSet = BinaryCharacterSetMapper()
+
+
+while(True):
+	user_command = str(raw_input("Input next character. Type 'exit' to stop:"))
+	if user_command=="exit":
+		break
+	else:
+		leftStepperReady = threading.Event()
+		rightStepperReady = threading.Event()
+		if not left_motor_thread.isAlive():
+			left_motor_thread = threading.Thread(target=changeStepperState, args=(leftStepper, str(user_command), BINARY_charSet, 0,leftStepperReady,))
+			left_motor_thread.start()
+		if not right_motor_thread.isAlive():
+			right_motor_thread = threading.Thread(target=changeStepperState, args=(rightStepper, str(user_command), BINARY_charSet, 1, rightStepperReady,))
+			right_motor_thread.start()
+		punchBraille(leftStepperReady, rightStepperReady, brailleEmbosser)
+	time.sleep(1)
